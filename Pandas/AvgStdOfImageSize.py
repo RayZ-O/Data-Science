38912714032be658bb40cc69db4aeee487135717
Rui Zhang <rayzhang1011@gmail.com>
2015-09-04 21:37:56 -0400

    Print the average file size and the standard deviation for images (.gif, .jpg and .jpeg files), which had response code 200

---
+++ Pandas/AvgStdOfImageSize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pylab import *
+import pandas as pd
+import re
+
+# Print the average file size and the standard deviation for images 
+# (.gif, .jpg and .jpeg files), which had response code 200
+
+# file format example: 
+# 1044,30/Apr/1998,22:46:12,/images/11104.gif,200,508
+# log_df = pd.read_csv('/home/datascience/Desktop/small.csv',
+log_df = pd.read_csv('/home/datascience/Desktop/wc_day6_1_sample.csv',
+names=['ClientID','Date','Time','URL','ResponseCode','Size'],
+na_values=['-'])
+
+r200_img_df = log_df[ (log_df['ResponseCode'] == 200) &
+             (log_df['URL'].str.contains('.gif$|.jpg$|.jpeg$', 
+             flags=re.IGNORECASE, regex=True, na=False))]
+
+print 'Average:', r200_img_df['Size'].mean(axis=1)
+print 'Standard deviation:', r200_img_df['Size'].std(axis=1)
--- Pandas/DatetimeFilter.py
+++ Pandas/DatetimeFilter.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # file format example: 
 # 1044,30/Apr/1998,22:46:12,/images/11104.gif,200,508
-# log_df = pd.read_csv('/home/datascience/Desktop/small.csv',
+
 log_df = pd.read_csv('/home/datascience/Desktop/wc_day6_1_sample.csv',
 names=['ClientID','Date','Time','URL','ResponseCode','Size'],
 na_values=['-'])
