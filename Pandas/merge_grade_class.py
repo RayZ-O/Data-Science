ab82996352207dc86c60aca56c0d396a5787a564
Rui Zhang <rayzhang1011@gmail.com>
2015-09-17 15:19:36 -0400

    Print out only the course Alice got A

--- Pandas/merge_grade_class.py
+++ Pandas/merge_grade_class.py
@@ -8,9 +8,11 @@ Students = pd.DataFrame({'student_id': [1, 2], 'name': ['Alice', 'Bob']})
 Grades = pd.DataFrame({'student_id': [1, 1, 2, 2], 'class_id': [1, 2, 1, 3], 'grade': ['A', 'C', 'B', 'B']})
 Classes = pd.DataFrame({'class_id': [1, 2, 3], 'title': ['Math', 'English', 'Spanish']})
 
-print pd.merge(pd.merge(Students, Grades, on='student_id'), Classes, on='class_id')
-#     name  student_id  class_id grade    title
-# 0  Alice           1         1     A     Math
-# 1    Bob           2         1     B     Math
-# 2  Alice           1         2     C  English
-# 3    Bob           2         3     B  Spanish
+Classgrade = pd.merge(Grades, Classes, on='class_id')
+alice_id = Students[Students['name'] == 'Alice'].at[0, 'student_id']
+course = Classgrade[(Classgrade['student_id'] == alice_id) & (Classgrade['grade'] == 'A')]['title']
+print course
+
+# 0    Math
+# Name: title, dtype: object
+
