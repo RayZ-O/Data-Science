d11785f8de4a5565ed63041aa1fc5e633f9e7c36
Rui Zhang <rayzhang1011@gmail.com>
2015-09-23 12:06:46 -0400

    Added comments about infobox format

--- NLP/extract_wikipedia_infobox.py
+++ NLP/extract_wikipedia_infobox.py
@@ -8,7 +8,7 @@ import sys
 
 title='Barack Obama'
 # title='Jim Parsons'
-# title='Xi Jinping'
+title='Xi Jinping'
 response = requests.get('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='+str(title)+'&prop=revisions&rvprop=content')
 jsondata = response.json()
 content = jsondata['query']['pages'].values()[0]['revisions'][0]['*']
@@ -16,31 +16,38 @@ content = jsondata['query']['pages'].values()[0]['revisions'][0]['*']
 wikicode = mwph.parse(content)
 infobox = ''
 for t in wikicode.filter_templates():
-	if re.search(re.compile('infobox', re.IGNORECASE), t.encode('utf-8')):
+	if re.search(re.compile('infobox ', re.IGNORECASE), t.encode('utf-8')):
 	 	infobox = t.encode('utf-8')
-print infobox
 if not infobox:
 	print "Can't find infobox."
 	sys.exit(1)
 
 print title + ':\n'
-spouse = re.search(r'spouse.*\[\[([^\]]*)\]\]', infobox)
+# spouse = {{marriage|[[Michelle Obama|Michelle Robinson]]|October 3, 1992}}
+# spouse         = Ke Lingling (divorced)<br>[[Peng Liyuan]] (m. 1987)
+spouse = re.search(r'spouse[^\[]*\[\[([^\]]*)\]\]', infobox)
 if spouse:
 	print 'Spouse:', spouse.group(1), '\n' 
 else:
 	print 'Spouse not found\n'
-
+# birth_place = [[Honolulu]], [[Hawaii]], U.S.
+# birth_place    = [[Beijing]], [[China]]
 raw_birth_place = re.search(r'birth_place[ ]*=[ ]*(.*)', infobox)
 if raw_birth_place:
 	print 'Place of birth:', re.sub('\[|\]', '', raw_birth_place.group(1)), '\n'
 else:
 	print 'Place of birth not found\n'
-
-education = re.search(r'education.*\[\[([^\]]*)\]\]', infobox)
+# education = [[Punahou School]]
+education = re.search(r'education[ ]*=[ ]*\[\[([^\]]*)\]\]', infobox)
 if education:
 	print 'Education:', education.group(1), '\n'
-
 # schools maybe {{plain list|\n*[[school A]]\n*[[school B]]\n*[[school B]]\n}} or single element [[school A]]
+# alma_mater = {{plain list|
+# *[[Occidental College]]
+# *[[Columbia University]] <small>(B.A.)</small>
+# *[[Harvard Law School]] <small>(J.D.)</small>
+# }}
+# alma_mater     = [[Tsinghua University]]
 raw_alma_mater = re.search(re.compile(r'alma_mater[ ]*=[ ]*({{plain list([^}]*)|\[\[([^\]]*\]\]))', re.DOTALL), infobox)
 if raw_alma_mater:
 	print 'Schools:'
