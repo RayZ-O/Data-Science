a6726bc666ba23e0ad53bb8b5a6cbbb10eef8af0
RayZ_O <rayzhang1011@gmail.com>
2015-10-13 19:45:36 -0400

    Save data to disk

--- Crawler/amazon_beauty.py
+++ Crawler/amazon_beauty.py
@@ -2,35 +2,45 @@
 
 import urllib.request
 from bs4 import BeautifulSoup
-import re, io, gzip
+import re, io, gzip, os, time
 
-headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) \
-                        Chrome/45.0.2454.101 Safari/537.36',
-        'Accept':' text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Encoding': 'gzip'}
+result_path = '/home/rui/Desktop/Skin/'
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+         'Accept':' text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+         'Accept-Encoding': 'gzip'}
 
 base_url = r'http://www.amazon.com'
 search_url = r'http://www.amazon.com/s/url=search-alias%3Dbeauty&field-keywords='
 
 def fetch_html(url, retry):
+    attempts = 0
     while attempts < retry:
         try:
-            req = urllib.request.Request(url, headers)
+            req = urllib.request.Request(url, headers=headers)
             response = urllib.request.urlopen(req)
+            # accept encoding is gzip, decode to get the actual html
             bi = io.BytesIO(response.read())
             gf = gzip.GzipFile(fileobj=bi, mode='rb')
             return gf.read()
-        except e:
+
+        except Exception as e:
+            print(str(e))
             attempts += 1
-            print("Fetch html error")
+            time.sleep(1)
     return ""
 
 def fetch_refine_links(soup):
     skin_types = ['Combination', 'Normal', 'Dry', 'Oily']
     refine_lst = soup.findAll('span', {'class' : 'refinementLink'})
-    return [ r.parent['href'] for r in refine_lst if r.string in skin_types ]
+    refine_links = {}
+    for r in refine_lst:
+        if r.string in skin_types:
+            # link to the refinement page is in the parent tag
+            refine_links[r.string] = r.parent['href']
+    return refine_links
 
 def fetch_products(soup):
+    # get ancestor tag of each product
     result_lst = soup.findAll(id=re.compile('result_\d+'))
     products = []
     for result in result_lst:
@@ -38,32 +48,40 @@ def fetch_products(soup):
         products.append(h2_tag.contents[0])
     return products
 
+def save_as(filename, products):
+    text_file = open(filename , "w")
+    for p in products:
+        text_file.write(p + '\n')
+    text_file.close()
+
 keywords = ['cleanser', 'moisturizers']
 for keyword in keywords:
+    prod_dir = result_path + keyword + '/'
+    if not os.path.exists(prod_dir):
+        os.makedirs(prod_dir)
     # fetch start html
-    html = fetch_html(search_url + keyword, 3)
-    soup = BeautifulSoup(html, 'lxml')
+    start_page_html = fetch_html(search_url + keyword, 3)
+    soup = BeautifulSoup(start_page_html, 'lxml')
 
     # get links to refinement
     refine_links = fetch_refine_links(soup)
 
-    for link in refine_links:
+    for skin_type, link in refine_links.items():
+        # create a directory for corresponding type
+        type_dir = prod_dir + skin_type + '/'
+        if not os.path.exists(type_dir):
+            os.makedirs(type_dir)
 
         url = base_url + link
-
+        page_num = 1
         while url != base_url:
-            html = fetch_html(url, 3)
-            soup = BeautifulSoup(html, 'lxml')
-
-            # fetch all products' name in this page
+            product_page_html = fetch_html(url, 3)
+            soup = BeautifulSoup(product_page_html, 'lxml')
+            # fetch all products' name in this page and save
             products = fetch_products(soup)
-            # TODO save products to file
-
+            save_as(type_dir + 'page' + str(page_num), products)
+            page_num += 1
             # get link to next page
-            next_page_link = soup.find(id='pagnNextLink')['href']
+            next_page = soup.find(id='pagnNextLink') # last page id is pagnNextString
+            next_page_link = next_page['href'] if next_page is not None else ''
             url = base_url + next_page_link
-
-
-
-
-
